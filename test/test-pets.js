3d133c0503f5d3bb7a593457c4f64234c46c662a
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-10-07 14:18:28 -0700

    Implemented responding to JSON

--- test/test-pets.js
+++ test/test-pets.js
@@ -99,6 +99,19 @@ describe('Pets', ()  => {
      });
   });
 
+// TEST LIST SEARCH
+  it('should list ALL pets on /pets GET', function(done) {
+    chai.request(server)
+        .get('/')
+        .set('content-type', 'application/json')
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          done();
+        });
+  });
+
 
   // TEST UPDATE
   it('should update a SINGLE pet on /pets/<id> PUT', (done) => {
